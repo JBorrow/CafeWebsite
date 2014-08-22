19c6b2e9a44f62b0f7ae3a482ec504a25d4e07d2
JBorrow <joshua.borrow@durham.ac.uk>
2014-08-22 12:15:27 +0100

    Added carrie ambler

---
+++ Upcoming/next.php
@@ -0,0 +1,7 @@
+<h1>Epidermal Cell Biology</h1>
+
+<img src = "http://imagizer.imageshack.us/v2/150x100q90/59/m15tricome.jpg" 
+alt = "Picture: Epidermal cells of a leaf" />
+
+<p>Here, Dr. Carrie Ambler will be giving a talk. Her main research interest is
+understanding how cell-cell interactions regulate skin stem cells.</p>
