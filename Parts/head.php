f862b179a41dfb7c1a03209d6ff5e257e8fd5cd6
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-08-24 20:20:57 +0100

    Changed favicon to hopefully a format that works

--- Parts/head.php
+++ Parts/head.php
@@ -3,6 +3,9 @@
     <meta charset="utf-8">
     <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
     <link rel = "stylesheet" type = "text/css" href = "stylesheet.css"/>
-    <link rel="icon" href="../CafeFavicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="http://www.cafescidurham.co.uk/favicon.ico" 
+    type="image/x-icon"/>
+    <link rel="shortcut icon" href="http://www.cafescidurham.co.uk/favicon.ico"
+    type = "image/x-icon" />
 </head>
 
