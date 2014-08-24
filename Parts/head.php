ed5993d974c795f7e498f2770562de6a2185d0fa
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-08-24 20:15:06 +0100

    Added favicon

--- Parts/head.php
+++ Parts/head.php
@@ -3,5 +3,6 @@
     <meta charset="utf-8">
     <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
     <link rel = "stylesheet" type = "text/css" href = "stylesheet.css"/>
+    <link rel="icon" href="../CafeFavicon.ico" type="image/x-icon"/>
 </head>
 
