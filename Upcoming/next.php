6ca33deaac48c738025e6e85138d1d5bb4f35506
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-08-24 19:20:44 +0100

    changed image

--- Upcoming/next.php
+++ Upcoming/next.php
@@ -1,6 +1,6 @@
 <h1>Epidermal Cell Biology</h1>
 
-<img src = "http://img59.imageshack.us/img59/8914/m15tricome.jpg" 
+<img src = "http://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Leaf_epidermis_w_scale.jpg/1280px-Leaf_epidermis_w_scale.jpg" 
 alt = "Picture: Epidermal cells of a leaf" />
 
 <p>Here, Dr. Carrie Ambler will be giving a talk. Her main research interest is
