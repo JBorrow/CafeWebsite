d91aeefc5dd1ed7dc5fa6eae3dcbb1565071f5fe
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-09-06 12:59:47 +0100

    Added more detail

--- Upcoming/next.php
+++ Upcoming/next.php
@@ -5,5 +5,17 @@ alt = "Picture: Epidermal cells of a leaf" />
 
 <h2>11th October 2014</h2>
 
-<p>Here, Dr. Carrie Ambler will be giving a talk. Her main research interest is
-understanding how cell-cell interactions regulate skin stem cells.</p>
+<p>This week, Carrie Ambler from the biology department here in Durham will give
+a talk on the cellular nature of skin and regeneration. Afterwards, there will
+be an informal discussion on science news that has occurred over the summer.</p>
+
+<p>As always, we will be meeting in the Empty Shop, which is just outside The
+Gates Shopping Centre. There will be a board outside so you know where you are
+going! The 'doors' will open at 3:45 pm and the talk will be starting at 4:00
+pm. See you there!</p>
+
+<p>Everyone is welcome at Cafe Scientifique - be you a student, a member of
+staff from the University or just a member of the public with an interest in
+science! See you there for a fun and intersting talk!</p>
+
+
