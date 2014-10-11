80e9356feafca07f742e6dbb7d0a88b93e33120d
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-10-11 17:27:09 +0100

    moved carrie out

---
+++ Previous/2014/October/11.php
@@ -0,0 +1,33 @@
+<h1>Epidermal Cell Biology</h1>
+
+<img src = "http://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Leaf_epidermis_w_scale.jpg/1280px-Leaf_epidermis_w_scale.jpg" 
+alt = "Picture: Epidermal cells of a leaf" />
+
+<h2>11th October 2014</h2>
+
+<p>This week, Carrie Ambler from the biology department here in Durham will give
+a talk on the cellular nature of skin and regeneration. Afterwards, there will
+be an informal discussion on science news that has occurred over the summer.</p>
+
+<p>You can join the Facebook event
+<a href="https://www.facebook.com/events/377779525711184/">on the Cafe group
+here</a>.</p>
+
+<h1>This Cafe starts early at 3:00 pm</h1>
+
+<p>Here's the abstract to the talk:</p>
+
+<blockquote>
+We are going to explore the world of skin regeneration and repair. Mammals, unlike some amphibians, are poor at regenerating adult tissues. We’ll explore the reasons behind this and discuss how studying regeneration in newts, axototls, and even flatworms is aiding biomedical research in promoting better healing in humans.
+</blockquote>
+
+<p>As always, we will be meeting in the Empty Shop, which is just outside The
+Gates Shopping Centre. There will be a board outside so you know where you are
+going! The 'doors' will open at 3:00 pm and the talk will be starting at 3:15
+pm. See you there!</p>
+
+<p>Everyone is welcome at Cafe Scientifique - be you a student, a member of
+staff from the University or just a member of the public with an interest in
+science! See you there for a fun and intersting talk!</p>
+
+
