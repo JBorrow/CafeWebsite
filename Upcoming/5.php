933336947b20e5a74e714ed94d658612a76d6d0e
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-10-27 21:10:58 +0000

    Added Chris Groves

---
+++ Upcoming/5.php
@@ -0,0 +1,29 @@
+<h1>Plastic Fantastic</h1>
+
+<img src="http://yuenkong.com/blog/wp-content/uploads/2011/06/Yuenkong-ltd-plastic-injection-material.jpg"
+     alt="Picture of plastic" />
+
+<h2>29th November 2014</h2>
+
+<p>Dr. Chris Groves from the Engineering department in Durham is coming to talk
+about plastics, how they've shaped the past and how they're going to shape the
+future. Here's the abstract for his talk:</p>
+
+<blockquote>
+    No other invention of the 20th century has impacted everyday life quite like the transistor.
+    The rapid spread of the computer from research labs, to offices, to homes, to your phone,
+    watch or glasses would not be possible without it.  This is largely made possible using a
+    single material – Silicon.  However, researchers at Durham and elsewhere are examining new
+    breeds of electronic materials based on plastics.  These materials offer the chance to do
+    something that Silicon cannot – the chance to change the colour of light they can emit and
+    absorb, as well as be printed as easily and cheaply as a newspaper.  These capabilities
+    make plastics exciting new materials in a variety of applications stretching from televisions
+    to solar cells.  In this talk I will discuss what makes these plastics so special, and why
+    that may lead to another revolution in electronics.
+</blockquote>
+
+<p>We are
+    looking forward to seeing you there at the usual time of 3:45 pm in Empty Shop!
+    If you're unsure how to get there, please take a look at our
+    <a href="index.php">homepage</a> where there is a map. On the day, we will put
+    a blackboard outside at 3:30 to guide you in!</p>
