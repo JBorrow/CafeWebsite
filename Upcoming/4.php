87dbaf30763959b4b826c68e3c456cfe65c5bbcb
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-11-12 22:01:43 +0000

    added james blowey's talk

---
+++ Upcoming/4.php
@@ -0,0 +1,18 @@
+<h1>Rubik's Cube Bonanza</h1>
+
+<img src="http://upload.wikimedia.org/wikipedia/commons/9/9a/Rubik's_Cube_variants.jpg" alt="A picture of a bunch of wonderful cubes" />
+
+<p>Dr. James Blowey, the director of Natural Sciences at Durham University, will
+be giving a talk on Rubik's cube. He will aim to answer such philisophical
+questions such as "Why do the media find Rubik's cube fascinating?".</p>
+
+<p>James has a sizeable collection of Rubik's cubes, and it could be said that
+he's a collector. Come along to learn something new about these fantastic
+puzzles!</p>
+
+<p>We are
+    looking forward to seeing you there at the usual time of 3:45 pm in Empty Shop!
+	    If you're unsure how to get there, please take a look at our
+		    <a href="index.php">homepage</a> where there is a map. On the day, we will put
+			    a blackboard outside at 3:30 to guide you in!</p>
+
