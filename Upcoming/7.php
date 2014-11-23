eaa9f3c163b811b7adeeb654aa1fe53073734b0e
JBorrow <joshua.borrow@durham.ac.uk>
2014-11-23 12:34:04 +0000

    added diner sci

---
+++ Upcoming/7.php
@@ -0,0 +1,16 @@
+<h1>Diner Scientifique December</h1>
+
+<img src="http://abuelostaqueria.com/wp-content/uploads/TexMex_Taco_Dinner.jpg" alt="some lovely tex mex food" />
+
+<h2>13th December 2014</h2>
+
+<p>
+Hi everyone, this week we will be going to a restaurant rather than meeting in Empty Shop, swapping
+our caffine for carbohydrates! If you're interested in coming, please e-mail me below before the
+1st December so we can book the table. As for a choice of restaurant, we'll probably be going to
+Tia's - a little tex-mex place in the center of town.
+</p>
+
+<p>E-mail: <a href="mailto:josh@cafescidurham.co.uk">josh@cafescidurham.co.uk</a></p>
+
+<p>Thanks!</p>
